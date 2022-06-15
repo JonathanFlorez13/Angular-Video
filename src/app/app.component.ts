d39c949175e378055ffcefc70d70a023ad2b6767
Jonathan Florez <jflorez@godigital.com.co>
2022-06-15 10:41:42 -0500

    try to change background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import * as OT from '@opentok/client';
 import { VirtualBackgroundEffect } from '@vonage/video-effects/dist/effects/background/VirtualBackgroundEffect';
-import { BackgroundEffectProcessor,  } from '@vonage/video-effects/dist/effects/BackgroundEffectProcessor';
+import { BackgroundEffectProcessor } from '@vonage/video-effects/dist/effects/BackgroundEffectProcessor';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -15,12 +15,13 @@ export class AppComponent {
   token =
     'T1==cGFydG5lcl9pZD00Njc5NzU2NCZzaWc9NGYzYmRmOGI5MDI5ZjNiYzFiZDEwZjhjZmVlN2JjYmM2ODgyMGIzNjpzZXNzaW9uX2lkPTJfTVg0ME5qYzVOelUyTkg1LU1UWTFOVEl4TmpRMU9EUTNNWDQwV0VZdlkycExlRTlxTlhVMFdEWnZaM1JuWkRka2JVUi1mZyZjcmVhdGVfdGltZT0xNjU1MjE2NDU5Jm5vbmNlPTAuMzkyNTIyMzE0OTExNDM3NjMmcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTY1NTMwMjg1OSZpbml0aWFsX2xheW91dF9jbGFzc19saXN0PQ==';
 
-  async loadImage():Promise<HTMLImageElement> {
+  async loadImage(): Promise<HTMLImageElement> {
     return new Promise((resolve) => {
-      const image = document.createElement('img');
-      image.crossOrigin = 'anonymous';
-      image.src = `backgrounds/bookshelf.jpg`;
-      image.onload = () => resolve(image)
+      const image = new Image();
+      //const image = document.createElement('img');
+      // image.crossOrigin = ' ';
+      image.src = 'Users/jflorez/Documents/Angular/prueba/src/app/backgrounds/simpson_background.jpg';
+      image.onload = () => resolve(image);
     });
   }
 
@@ -38,16 +39,31 @@ export class AppComponent {
 
   async createBackgroudEffectProcessor() {
     const effectProcessor = new BackgroundEffectProcessor({
-      assetsPath: './assets/bookshelf.jpg',
+      assetsPath: 'https://d7ca6333nyzk0.cloudfront.net/',
+      frameRate: 30,
     });
     effectProcessor.setInputStream(await this.getLocalMedia());
     return effectProcessor;
   }
 
   async video() {
-
-
+    const session = OT.initSession(this.apikey, this.sessionId);
     const effectProcessor = await this.createBackgroudEffectProcessor();
+
+    const publisher = OT.initPublisher(
+      'publisher',
+      {
+        videoSource: effectProcessor.outputStream.getVideoTracks()[0],
+        publishAudio: false,
+        publishVideo: true,
+        insertMode: 'append',
+      },
+      (err) => {
+        if (err) console.error('Error publishing:', err);
+        else console.log('Publisher Created');
+      }
+    );
+
     await effectProcessor.loadEffect(
       new VirtualBackgroundEffect({
         virtualBackground: {
@@ -58,12 +74,6 @@ export class AppComponent {
     );
     effectProcessor.pauseStreamProcessing(false);
     effectProcessor.enableEffect(true);
-    const session = OT.initSession(this.apikey, this.sessionId);
-    const publisher = OT.initPublisher('publisher', {
-      videoFilter: {
-        type: 'backgroundBlur',
-      },
-    });
 
     session.on({
       streamCreated: (event: any) => {
@@ -71,10 +81,7 @@ export class AppComponent {
       },
       sessionConnected: async (event: any) => {
         session.publish(publisher);
-        await publisher.applyVideoFilter({
-          type: 'backgroundBlur',
-          blurStrength: 'low',
-        });
+        effectProcessor.pauseStreamProcessing(!event.event.newValue);
       },
     });
 
